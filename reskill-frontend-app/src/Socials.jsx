e5bd77ae38aa29d1e1d1f1fd58dd411b4090dcf6
Athanasios Nakas <anakas@deloitte.gr>
2024-06-14 12:12:40 +0300

    Fixed links in social buttons

--- reskill-frontend-app/src/Socials.jsx
+++ reskill-frontend-app/src/Socials.jsx
@@ -1,4 +1,3 @@
-
 import fbLogo from './assets/fb.svg'
 import linkedLogo from './assets/linkedIn.svg'
 import youtubeLogo from './assets/youtube.svg'
@@ -7,10 +6,10 @@ import instaLogo from './assets/instagram.svg'
 function Socials() {
   return (
     <div className='rat-flex'>
-      <div className='rat-mr-3'><a href=''><div className='p-3 border-2 border-transparent hover:border-current'><img src={fbLogo} width='24' /></div></a></div>
-      <div className='rat-mr-3'><a href=''><div className='p-3 border-2 border-transparent hover:border-current'><img src={linkedLogo} width='24' /></div></a></div>
-      <div className='rat-mr-3'><a href=''><div className='p-3 border-2 border-transparent hover:border-current'><img src={youtubeLogo} width='24' /></div></a></div>
-      <div className='rat-mr-3'><a href=''><div className='p-3 border-2 border-transparent hover:border-current'><img src={instaLogo} width='24' /></div></a></div>
+      <div className='rat-mr-3'><a href='https://www.facebook.com/'><div className='p-3 border-2 border-transparent hover:border-current'><img src={fbLogo} width='24' /></div></a></div>
+      <div className='rat-mr-3'><a href='https://www.linkedin.com/'><div className='p-3 border-2 border-transparent hover:border-current'><img src={linkedLogo} width='24' /></div></a></div>
+      <div className='rat-mr-3'><a href='https://www.youtube.com/'><div className='p-3 border-2 border-transparent hover:border-current'><img className='rat-py-1' src={youtubeLogo} width='24' /></div></a></div>
+      <div className='rat-mr-3'><a href='https://www.instagram.com/'><div className='p-3 border-2 border-transparent hover:border-current'><img src={instaLogo} width='24' /></div></a></div>
     </div>
   )
 }
